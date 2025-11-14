4d29c5bccd7c3bb17d192f017d8d98924040c683
mrDinosaur410 <timur.titov.04@mail.ru>
2025-11-14 18:22:36 +0300

    break integr

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -10,4 +10,4 @@ def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     data = response.json()
-    assert data["status"] == "Hello World OK"
+    assert data["status"] == "Hello World no break"

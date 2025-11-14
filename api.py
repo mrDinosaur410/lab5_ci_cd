f67ee069b890176fe54550b53155a2807caf07c3
mrDinosaur410 <timur.titov.04@mail.ru>
2025-11-14 17:13:06 +0300

    last

--- api.py
+++ api.py
@@ -300,7 +300,7 @@ async def data_2mb_route(current_user: User = Depends(require_scope(TokenScope.d
 
 @router.get("/data_10mb")
 async def data_10mb_route(current_user: User = Depends(require_scope(TokenScope.data_10mb))):
-    return {"message": f"Welcome, {current_user.username}. You have access to 10MB data scope."}
+    return {"message": f"Welcome, {current_user.username}. You have access to 10MB data scope!."}
 
 
 @router.get("/data_unlimited")
